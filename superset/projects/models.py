77d1746600c5b6e00ede9376dff1dc97fca37328
杨德贵 <15680553673@163.com>
2024-08-21 15:09:54 +0800

    add: add Project and UserProject model.

---
+++ superset/projects/models.py
@@ -0,0 +1,37 @@
+from flask_appbuilder import Model
+from sqlalchemy import (
+    Column,
+    Integer,
+    String,
+    ForeignKey,
+    UniqueConstraint,
+    Text,
+)
+from sqlalchemy.orm import relationship
+
+from superset.models.core import Database
+
+
+class Project(Model):
+    __tablename__ = "ab_project"
+
+    id = Column(Integer, primary_key=True)
+    project_id = Column(String(256))
+    name = Column(String(256), nullable=False)
+    project_name = Column(String(256), index=True)
+    attrs = Column(Text, nullable=True)
+
+
+class UserProject(Model):
+    __tablename__ = "ab_user_project"
+
+    id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey("ab_user.id"), nullable=False)
+    project_id = Column(Integer, ForeignKey("ab_project.id"), nullable=False)
+
+    user = relationship("User")
+    project = relationship("Project")
+
+    __table_args__ = (
+        UniqueConstraint('user_id', 'project_id', name='user_project_unique_constraint')
+    )
