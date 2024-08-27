aba33bc22be701deddf406420426052218bf3519
杨德贵 <15680553673@163.com>
2024-08-27 14:15:43 +0800

    add: add project correlation table, correlation include DASHBOARD, SLICE, DATASET, USER

--- superset/projects/models.py
+++ superset/projects/models.py
@@ -6,10 +6,11 @@ from sqlalchemy import (
     ForeignKey,
     UniqueConstraint,
     Text,
+    Enum,
 )
 from sqlalchemy.orm import relationship
 
-from superset.models.core import Database
+from superset.models.helpers import AuditMixinNullable
 
 
 class Project(Model):
@@ -35,3 +36,37 @@ class UserProject(Model):
     __table_args__ = (
         UniqueConstraint('user_id', 'project_id', name='user_project_unique_constraint')
     )
+
+
+class ProjectCorrelationType(Enum):
+    DASHBOARD = "dashboard"
+    SLICE = "slice"
+    DATASET = "dataset"
+    USER = "user"
+
+
+class ProjectCorrelationObject(Model, AuditMixinNullable):
+    """An association between an object and a tag."""
+
+    __tablename__ = "project_correlation_object"
+    id = Column(Integer, primary_key=True)
+    project_id = Column(Integer, ForeignKey("ab_project.id"))
+    object_id = Column(
+        Integer,
+        ForeignKey("dashboards.id"),
+        ForeignKey("slices.id"),
+        ForeignKey("datasets.id"),
+        ForeignKey("ab_user.id"),
+    )
+    object_type = Column(Enum(ProjectCorrelationType))
+
+    project = relationship("Project", back_populates="objects")
+
+    __table_args__ = (
+        UniqueConstraint(
+            "project_id", "object_id", "object_type", name="uix_pco"
+        ),
+    )
+
+    def __str__(self) -> str:
+        return f"<TaggedObject: {self.object_type}:{self.object_id} TAG:{self.project_id}>"
