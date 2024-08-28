a67c4b999922289fe5768a2166012a21eb0fa096
杨德贵 <15680553673@163.com>
2024-08-28 12:03:21 +0800

    add: add get_list_by_manager, change_project at ProjectRestApi

--- superset/projects/api.py
+++ superset/projects/api.py
@@ -17,15 +17,18 @@
 # pylint: disable=too-many-lines
 import logging
 
-from flask import Response
+from flask import Response, g, request, session
 from flask_appbuilder import has_access, permission_name
 from flask_appbuilder.api import expose, safe
 from flask_appbuilder.models.sqla.interface import SQLAInterface
 from flask_appbuilder.security.decorators import protect
+from flask_jwt_extended.exceptions import NoAuthorizationError
 
-from superset import event_logger, db
+from superset import event_logger, db, is_feature_enabled
 from superset.constants import RouteMethod, MODEL_API_RW_METHOD_PERMISSION_MAP
+from superset.daos.project import ProjectDAO
 from superset.projects.models import Project
+from superset.utils.core import get_project_id, set_project
 from superset.views.base_api import (
     BaseSupersetModelRestApi, requires_json, statsd_metrics,
 )
@@ -42,6 +45,7 @@ class ProjectRestApi(BaseSupersetModelRestApi):
     method_permission_name = MODEL_API_RW_METHOD_PERMISSION_MAP
     include_route_methods = RouteMethod.REST_MODEL_VIEW_CRUD_SET | {
         "get_list_by_manager",
+        "change_project",
     }
     list_columns = ["id", "name", "project_name", ]
     edit_columns = ["project_name"]
@@ -71,4 +75,42 @@ class ProjectRestApi(BaseSupersetModelRestApi):
         log_to_statsd=False,
     )
     def get_list_by_manager(self) -> Response:
-        return self.response(400, message="todo: implement")
+        try:
+            if g.user is None or g.user.is_anonymous:
+                return self.response_401()
+        except NoAuthorizationError:
+            return self.response_401()
+        projects = ProjectDAO.get_list_by_user(g.user)
+        current_project = get_project_id(request)
+        if not current_project and is_feature_enabled("USE_PROJECT"):
+            current_project = projects[0].id if projects else None
+            set_project(request, current_project)
+        return self.response(200, data={
+            "projects": list(
+                map(lambda x: {"id": x.id, "project_name": x.project_name}, projects)),
+            "current_project": current_project
+        })
+
+    @expose("/change_project/<int:project_id>", methods=("GET",))
+    @safe
+    @statsd_metrics
+    @event_logger.log_this_with_context(
+        action=lambda self, *args,
+                      **kwargs: f"{self.__class__.__name__}.change_project",
+        log_to_statsd=False,
+    )
+    def change_project(self, project_id: int) -> Response:
+        try:
+            if g.user is None or g.user.is_anonymous:
+                return self.response_401()
+        except NoAuthorizationError:
+            return self.response_401()
+        # Replace with the actual logic to get the project_id
+        is_manager = ProjectDAO.is_manager(g.user, project_id)
+
+        if is_manager:
+            set_project(request, project_id)
+            # Set the session variable
+            return self.response(200, message="Project changed successfully")
+        else:
+            return self.response(400, message="Not authorized to change project")
