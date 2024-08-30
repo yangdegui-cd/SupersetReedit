399d09ae23b88b26448a25ab86da2ed89fc95246
杨德贵 <15680553673@163.com>
2024-08-30 11:04:41 +0800

    add: add get_projects_by_user at ProjectCorrelationDAO

--- superset/daos/project_correlation.py
+++ superset/daos/project_correlation.py
@@ -1,6 +1,7 @@
-from superset import is_feature_enabled, db
+from superset import is_feature_enabled
 from superset.daos.base import BaseDAO
-from superset.projects.models import ProjectCorrelationObject
+from superset.extensions import db
+from superset.projects.models import ProjectCorrelationObject, ProjectCorrelationType
 
 
 class ProjectCorrelationDAO(BaseDAO[ProjectCorrelationObject]):
@@ -18,4 +19,11 @@ class ProjectCorrelationDAO(BaseDAO[ProjectCorrelationObject]):
         db.session.add(correlation)
         return correlation
 
-
+    @staticmethod
+    def get_projects_by_user(user_id):
+        return (
+            db.session.query(ProjectCorrelationObject)
+            .filter(ProjectCorrelationObject.object_id == user_id)
+            .filter(ProjectCorrelationObject.object_type == ProjectCorrelationType.USER)
+            .all()
+        )
