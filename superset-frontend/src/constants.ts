35358eb6f8b6433d29abd675b72010386b26b75f
杨德贵 <15680553673@163.com>
2024-09-05 18:15:56 +0800

    add(web): add edit at url params

--- superset-frontend/src/constants.ts
+++ superset-frontend/src/constants.ts
@@ -111,6 +111,10 @@ export const URL_PARAMS = {
     name: 'focused_chart',
     type: 'number',
   },
+  edit: {
+    name: 'edit',
+    type: 'boolean',
+  },
 } as const;
 
 export const RESERVED_CHART_URL_PARAMS: string[] = [
